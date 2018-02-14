0637597d76afd33099a825bf25086f0a2e80a643
Fola <folaakingbe@yahoo.com>
2018-02-14 11:46:38 -0500

    took too long

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
 var app = express(); var PORT = 3000;
-app.get("/", function(req, res) { res.send("I love Git!"); });
+var path = require("path");
+app.get("/", function(req, res) { res.sendFile(path.join(__dirname, "index.html")); });
 app.listen(PORT, function() { console.log("App listening on PORT " + PORT); });
